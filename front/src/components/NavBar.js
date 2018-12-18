686b53ed7941587f664fcc6f1d128caf5f167943
Julien SANS <julien_ss@hotmail.fr>
2018-12-18 14:24:57 +0100

    corrections navbar et routeur

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -1,39 +1,31 @@
-import React from 'react';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Button } from 'reactstrap';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'reactstrap';
+import '../styles/NavBar.css';
 
-export default class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
+  export default class NavBar extends React.Component {
+    constructor(props) {
+      super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">STATS</NavbarBrand>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/"><Button color="success">LPH</Button></NavLink>
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
 
-              </NavItem>
-            </Nav>
+    render() {
+      return (
+        <Fragment>
+          <Navbar color="dark" className="fixed-top" expand="md">
+          <Link exact to='/Home' className="mr-3"><strong className="text-white">STATS</strong></Link>
+          <Link className="nav-item text-white" exact to="/lph-stats">LPH</Link>
         </Navbar>
-      </div>
+      </Fragment>
     );
   }
 }
